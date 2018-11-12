6045f924acc4e7908aa0139896150d74eaef83cb
MattRodigheri <mrodigheri@gmail.com>
2018-11-11 20:33:11 -0800

    showing day of week

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import moment from 'moment';
+import styles from '../styles/App.css';
 
 class App extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+
+    }
   }
 
   render() {
+    var day = moment().format('dddd');
+    var workout;
+    if (day === 'Sunday') {
+      workout = 'sunday'
+    }
+    if (day === 'Monday') {
+      workout = 'monday'
+    }
     return (
-      <div>testing</div>
+      <div>
+        <h1>{day}</h1>
+        <div>{workout}</div>
+      </div>
     )
   }
 }
